246781508e593ac3076a0c78b2b72732c9198d50
Mohammed Adel <MohammedAdel224@users.noreply.github.com>
2024-10-20 10:22:21 +0300

    4U Scans - fix _get_chapter_href

--- manga/for_u_scans.py
+++ manga/for_u_scans.py
@@ -35,6 +35,5 @@ class ForUScans(Manga):
     def _find_previous_chapter(chapter: element.Tag) -> element.Tag | None:
         return chapter.find_next_sibling()
     
-    @staticmethod
-    def _get_chapter_href(chapter: element.Tag) -> str:
-        return chapter['href']
+    def _get_chapter_href(self, chapter: element.Tag) -> str:
+        return self.url.split("/series/")[0] + chapter['href']
